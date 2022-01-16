92dc9570036b65705f9f4222680d61ff9f9b682f
StadiaPS <silvaalvares81@gmail.com>
2022-01-16 13:36:03 +0000

    try

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const screenSize = document.querySelector('p.screen-size');
+const r = document.querySelector(":root");
 
 let width = screen.width;
 let height = screen.height;
+
+r.style.setProperty('--device-width', width);
+r.style.setProperty('--device-height', height);
 
 screenSize.innerHTML = width + ' X ' + height;
